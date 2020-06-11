2b27f88734348033311bafd4fd47f1296ea53271
wajdi kanzali <wkanzali@maisonsdumonde.com>
2020-06-11 02:01:46 +0200

    add languages

---
+++ front/src/const/countries.js
@@ -0,0 +1,13 @@
+const LANGUAGES = [
+    {
+      label: 'Français',
+      value: 'fr',
+    },
+    {
+      label: 'Anglais',
+      value: 'en',
+    },
+  ];
+  
+export default LANGUAGES;
+  
